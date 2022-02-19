ff317e95147e7715dbd30e3e21c2d1297ed56b65
kevin95all <58482652+kevin95all@users.noreply.github.com>
2022-02-18 22:37:38 -0600

    Actualizando 18-02-22

--- Optimizador_de_Patrones/Main.py
+++ Optimizador_de_Patrones/Main.py
@@ -1,4 +1,63 @@
+from Operaciones import Operaciones
+
+
 class Main:
 
     def __init__(self):
+        self.ruta = ''
+        self.op = ''
+        self.salida = False
+        self.operaciones = Operaciones()
+
+    def menu_principal(self):
+        while not self.salida:
+            print('                                              ')
+            print('┌------------- MENU PRINCIPAL -------------┐')
+            print('|                                          |')
+            print('|          1)   Cargar archivo             |')
+            print('|          2)   Seleccionar piso           |')
+            print('|          3)   Mostrar piso               |')
+            print('|          4)   Seleccionar patrón         |')
+            print('|          5)   Mostrar patrón             |')
+            print('|          6)   Salir                      |')
+            print('|                                          |')
+            print('└------------------------------------------┘')
+            print('                                              ')
+
+            self.op = input('--> Ingrese una opción: ')
+
+            if self.op == '1':
+                pass
+
+            elif self.op == '2':
+                pass
+
+            elif self.op == '3':
+                pass
+
+            elif self.op == '4':
+                pass
+
+            elif self.op == '5':
+                pass
+
+            elif self.op == '6':
+                self.salir()
+
+            else:
+                print('                    ')
+                print('--> Opción no valida')
+
+    def cargar_archivo(self):
         pass
+
+    def salir(self):
+        print('                       ')
+        print('--> Programa finalizado')
+        self.ruta = ''
+        self.op = ''
+        self.salida = True
+
+
+app = Main()
+app.menu_principal()
